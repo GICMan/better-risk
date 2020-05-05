eaf0f8bdde091bdfda288a9f510a9e06eac45e89
Gabriel Casselman <gicasselman1@gmail.com>
2020-05-04 19:57:33 -0700

    auto detect port

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ io.on("connection", (socket) => {
   addEventListeners(io, socket);
 });
 
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port 3000");
 });
